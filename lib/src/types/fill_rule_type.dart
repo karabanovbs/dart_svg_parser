8e161f08f084f28f7a9141bd989a089f5c318ab4
Boris Karabanov <karabanovbswork@gmail.com>
2021-03-15 04:17:20 +0500

    [fix] evenodd enum name

--- lib/src/types/fill_rule_type.dart
+++ lib/src/types/fill_rule_type.dart
@@ -3,7 +3,7 @@ import 'package:json_annotation/json_annotation.dart';
 enum FillRuleType {
   @JsonValue('nonzero')
   nonzero,
-  @JsonValue('evened')
+  @JsonValue('evenodd')
   evenodd,
   @JsonValue('inherit')
   inherit,
